057f49aafc945bb081b7b8533b7f467d55161f99
kanium3 <dev@kanium.me>
2025-07-17 23:27:59 +0900

    変換機構の作成

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # design-token
 
 このプロジェクトで使用されるデザイントークンを管理するレポジトリです。
+
+## Development
+このレポジトリでは[`pnpm`](https://pnpm.io/)を使用しています。
+
+```shell
+pnpm install
+# design tokenを変換する
+pnpm run transform-tokens
+```
---
+++ transformTokens.ts
@@ -0,0 +1,30 @@
+import { StyleDictionary } from 'style-dictionary-utils'
+
+const myStyleDictionary = new StyleDictionary()
+
+const extendedSd = await myStyleDictionary.extend({
+    source: ['tokens/**/*.json'],
+    platforms: {
+        ts: {
+            transforms: ['color/hexAlpha', 'shadow/css'],
+            files: [
+                {
+                    filter: 'isSource',
+                    destination: 'tokens.ts',
+                    format: 'javascript/esm',
+                },
+            ],
+        },
+        css: {
+            transformGroup: "css",
+            files: [
+                {
+                    destination: "_variables.css",
+                    format: "css/variables",
+                },
+            ],
+        },
+    },
+})
+
+await extendedSd.buildAllPlatforms()
