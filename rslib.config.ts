564aeace0b0bb6f1f30c9ebc761c043b49da272c
kanium3 <dev@kanium.me>
2025-07-19 22:21:09 +0900

    CJS/ESMに両方対応
    rslibを導入しました

---
+++ rslib.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from '@rslib/core';
+
+export default defineConfig({
+  lib: [
+    {
+      format: 'esm',
+      syntax: ['node 18'],
+    },
+    {
+      format: 'cjs',
+      syntax: ['node 18'],
+    },
+  ],
+  output: {
+    target: "web",
+    distPath: {
+      root: "build/dist"
+    }
+  },
+  source: {
+    tsconfigPath: "./tsconfig.json",
+    entry: {
+      index: "build/ts/tokens.ts"
+    },
+  }
+});
